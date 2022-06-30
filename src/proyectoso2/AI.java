2b71380d1c57afb6962da7027c497a8f00bebf2f
Juan Isturiz <juan.isturiz@correo.unimet.edu.ve>
2022-06-30 18:28:10 -0400

    Listo read & write

--- src/proyectoso2/AI.java
+++ src/proyectoso2/AI.java
@@ -6,8 +6,6 @@ package proyectoso2;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,42 +13,39 @@ import javax.swing.JOptionPane;
  * @author mvlop
  */
 public class AI {
-    
-    public void iaProbability(Phones phone){
+
+    public void iaProbability(Phones phone) {
         Admin admin = new Admin();
-        
+
         int limit = 100;
-        
+
         Random probability = new Random();
         int probability2 = probability.nextInt(limit);
-        
+
         //"time" se debe poder modificar por infertaz 
         int time = 15;
-        
-        if(phone!=null){
-            
+
+        if (phone != null) {
+
             try {
-                TimeUnit.SECONDS.sleep(time); 
-                
+                TimeUnit.SECONDS.sleep(time);
+
             } catch (InterruptedException ex) {
                 JOptionPane.showMessageDialog(null, "Ha ocurrido un error inesperado con la AI", "ERROR", JOptionPane.ERROR_MESSAGE);
                 System.exit(1);
             }
-            
-            if( probability2 <= 40){
+
+            if (probability2 <= 40) {
                 //System.out.println("salió al mercado");
+                DataManage.writeData(phone);
                 JOptionPane.showMessageDialog(null, "Salió al mercado");
                 ProyectoSO2.released++;
-            }
-
-            else if( probability2 > 40 && probability2 <= 67 ){
+            } else if (probability2 > 40 && probability2 <= 67) {
                 //System.out.println("Empató");
                 JOptionPane.showMessageDialog(null, "Empató");
                 //falta crearla (nombre tentativo)
                 //admin.empEnqueue(phone);
-            }
-
-            else{
+            } else {
                 //System.out.println("Requiere refuerzos");
                 JOptionPane.showMessageDialog(null, "Requiere refuerzos");
                 admin.reEnqueue(phone);
@@ -58,5 +53,5 @@ public class AI {
         }
         ProyectoSO2.addingCounter++;
     }
-    
+
 }
---
+++ src/proyectoso2/DataManage.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoso2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author yunch
+ */
+public class DataManage {
+
+    public static boolean isInt(String n) {
+        try {
+            Integer.parseInt(n);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public int readTime() {
+        String path = "test\\time.txt";
+        File f = new File(path);
+        int Time = 2;
+        String line;
+        try {
+            FileReader fr = new FileReader(f);
+            try (BufferedReader br = new BufferedReader(fr)) {
+                
+                while ((line = br.readLine()) != null) {
+                    if (isInt(line)) {
+                        Time = Integer.parseInt(line);
+                        return Time;
+                    }
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error en la lectura de los datos iniciales", "ERROR", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+        return Time;
+    }
+
+    public static void writeData(Phones ph) {
+        String path = "test\\data.csv";
+        File f = new File(path);
+        try {
+            FileWriter fw = new FileWriter(f, true);
+            try (PrintWriter pw = new PrintWriter(fw, true)) {
+                pw.print(ph.id +";"+ph.plant+";"+ph.trophies);
+            }
+        } catch (IOException e) {
+
+        }
+
+    }
+
+}
