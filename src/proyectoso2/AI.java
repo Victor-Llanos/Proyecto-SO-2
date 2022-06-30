1479e4f06bfc876cc6e492c9a34949c8c22d63a8
Victor-Llanos <victor.llanos@correo.unimet.edu.ve>
2022-06-30 15:54:11 -0400

    logica ai

--- src/proyectoso2/AI.java
+++ src/proyectoso2/AI.java
@@ -4,10 +4,34 @@
  */
 package proyectoso2;
 
+import java.util.Random;
+
 /**
  *
  * @author mvlop
  */
 public class AI {
+    
+    public void iaProbability(Phones phone){
+        Admin admin = new Admin();
+        
+        int limit = 100;
+        
+        Random probability = new Random();
+        int probability2 = probability.nextInt(limit);
+        
+        if( probability2 <= 40){
+            System.out.println("salió al mercado");
+        }
+        
+        else if( probability2 > 40 && probability2 <= 67 ){
+            System.out.println("empató");
+        }
+        
+        else{
+            System.out.println("requiere refuerzos");
+        }
+        
+    }
     
 }
